ce674b0fdc5c24d2b3baff0d0035517d2166567e
Murat Artan <muratartan@hotmail.com>
2020-12-18 21:50:37 +0300

    add Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
+
 import './App.css';
+import Header from './containers/Header/Header';
 
 class App extends Component {
 
   render () {
     return (
       <div className="App">
-        hello shoki..! how are you?
+        <Header />
       </div>
     );
   }
