3691b0c186ae47a345b885db7c36f5d24bb89545
Murat Artan <muratartan@hotmail.com>
2020-12-18 23:12:44 +0300

    add logo and app name

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+
+import Logo from '../../components/Logo/Logo';
 import './Header.css';
 
 class Header extends Component {
     render () {
         return (
             <div className='Header'>
-                Logo  Navigations  github-TMDB
+                <Logo />  Navigations  github-TMDB
             </div>
         );
     }
