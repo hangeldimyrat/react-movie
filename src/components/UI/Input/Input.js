c808e3cb6ecaa22675928db86cf0b63667d58445
Murat Artan <muratartan@hotmail.com>
2020-12-20 18:35:55 +0300

    create global input component

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Input.css';
+
+const input = props => (
+    <div>
+        <input 
+            className={['Input' , props.InputType].join(' ')}
+            value={props.value}
+            onChange={props.changed}
+            placeholder={props.placeholder} />
+    </div>
+);
+
+export default input;
