1e124069feeee3a66f2376d4f95f9f86f82151db
Murat Artan <muratartan@hotmail.com>
2020-12-20 18:05:25 +0300

    create custom link component

---
+++ src/components/UI/Link/Link.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Link.css';
+
+const link = props => (
+    <a 
+        className='Link'
+        style={{fontSize:props.size, color:props.color}}
+        title={props.title}
+        href={props.url}
+        target='_blank'>
+            {props.children}
+    </a>
+);
+
+export default link;
--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
+import {FaGithub} from 'react-icons/fa';
 
 import Logo from '../../components/Logo/Logo';
 import './Header.css';
 import Navigation from '../../components/Navigation/Navigation';
+import Link from '../../components/UI/Link/Link';
 
 class Header extends Component {
     render () {
         return (
             <div className='Header'>
                 <Logo />  
-                <Navigation />  github-TMDB
+                <Navigation />  
+                <Link 
+                title='Github Repo'
+                size={23}
+                color='whitesmoke'
+                url='https://github.com/hangeldimyrat/react-movie'><FaGithub /></Link>
             </div>
         );
     }
