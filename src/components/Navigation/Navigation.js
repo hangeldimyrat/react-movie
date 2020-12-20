309d6309931e549c92e8c16b6b0cd291610f5518
Murat Artan <muratartan@hotmail.com>
2020-12-20 14:45:20 +0300

    add navigation items

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 import './Navigation.css';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigation = () => (
 
     <div className='Navigation'>
+        <NavigationItem title='Movies'>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+        </NavigationItem>
 
+        <NavigationItem title='TV shows'>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+        </NavigationItem>
+
+        <NavigationItem title='Genres'>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+        </NavigationItem>
     </div>
 );
 
--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 
 import Logo from '../../components/Logo/Logo';
 import './Header.css';
+import Navigation from '../../components/Navigation/Navigation';
 
 class Header extends Component {
     render () {
         return (
             <div className='Header'>
-                <Logo />  Navigations  github-TMDB
+                <Logo />  
+                <Navigation />  github-TMDB
             </div>
         );
     }
