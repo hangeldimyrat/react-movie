fa308a8ce1dfeb8d8906623efb19dc134edeca5c
Murat Artan <muratartan@hotmail.com>
2020-12-27 18:08:27 +0300

    set movie list content

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 import './MovieList.css';
+import Image from '../UI/Image/Image';
 
 const movieList = props => (
-    <div></div>
+    <div className='Container'>
+        <p className='Title'>Popular Movies</p>
+        <div className='List-Container'>
+            <div className='Movie-Container'>
+                <Image width={175}
+                       height={260}
+                       src={require('../../assets/poster.jpg').default}
+                       alt='poster'
+                       className='Poster-Image'
+                 />
+                 <div className='Movie-Vote-Container'>
+                     <p className='Movie-Star-Icon'>
+                         <i className='fas fa-star' />
+                     </p>
+                     <div>
+                         <p className='Movie-Vote-Average'>8.4</p>
+                     </div>
+                 </div>
+                 <p className='Movie-Title'>Interstellar</p>
+            </div>
+        </div>
+    </div>
 );
 
 export default movieList;
